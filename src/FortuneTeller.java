bf5c007e6fab4781af2001da547c3f74d4bf5bba
RivalJ <157822657+RivalJ@users.noreply.github.com>
2026-02-15 18:18:51 -0500

    basics of project set up, next phase is updating and polish

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+
 public class FortuneTeller {
     private FortuneTellerFrame frame;
+
+
     void main(String[] args){
         frame = new FortuneTellerFrame();
         frame.setVisible(true);
--- src/FortuneTellerFrame.java
+++ src/FortuneTellerFrame.java
@@ -1,41 +1,81 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class FortuneTellerFrame extends JFrame {
-    private JLabel fortuneLabel;
-    private JButton tellFortuneButton;
     private JPanel top, middle, bottom;
+    JTextArea textArea = new JTextArea(10,1);
+    JButton readButton = new JButton("Read My Fortune!");
+    JButton quitButton = new JButton("Quit");
+    ArrayList<String> fortunes = new ArrayList<>();
 
     public FortuneTellerFrame() {
         super.setTitle("Fortune Teller");
-        super.setSize(300, 200);
+        super.setSize(500, 300);
         super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         super.setLayout(new FlowLayout());
 
         SetUpTopPanel();
         SetUpMiddlePanel();
         SetUpBottomPanel();
+        CreateFortunes();
 
         super.setLocationRelativeTo(null);
     }
 
     private void SetUpTopPanel(){
         top = new JPanel();
-        JLabel label = new JLabel("Fortune Teller");//FIXME: this needs to have an image attached somehow
+        JLabel label = new JLabel("Fortune Teller");
+        label.setFont(new Font("Arial", Font.BOLD, 20));
+
+        Image icon = new ImageIcon(getClass().getResource("fortune.jpg")).getImage();
+        icon = icon.getScaledInstance(50,50,Image.SCALE_SMOOTH);
+        JLabel imageHolder = new JLabel(new ImageIcon(icon));
+
 
         top.add(label);
+        top.add(imageHolder);
         super.add(top);
-    }
+    }//FIXME: needs to dispaly the image above the text
     private void SetUpMiddlePanel(){
         middle = new JPanel();
-        JTextArea textArea = new JTextArea();
 
-        JScrollPane scrollPane = new JScrollPane(textArea);
-        scrollPane.add(textArea);//create scroll pane and add the text area to it
+        JScrollPane scrollPane = new JScrollPane(textArea);//create scroll pane and add the text area to it
+        scrollPane.setPreferredSize(new Dimension(250,textArea.getFont().getSize()*10));
+        textArea.setEditable(false);
+
 
         middle.add(scrollPane);
         super.add(middle);
-    }
+    }//FIXME: needs a font
     private void SetUpBottomPanel(){
+        bottom = new JPanel();
+
+        readButton.addActionListener(e -> textArea.append("\n" +GetRandomFortune()));
+        quitButton.addActionListener(e -> System.exit(0));
+
+        bottom.add(readButton);
+        bottom.add(quitButton);
+        super.add(bottom);
+    }//FIXME: needs a font
+
+    private String GetRandomFortune(){
+        return fortunes.get((int)(Math.random()*fortunes.size()));
+    }//FIXME: needs to not return the same fortune twice in a row
+    private void CreateFortunes(){
+        fortunes.add("You will have a great day!");
+        fortunes.add("You will be happy!");
+        fortunes.add("You will feel good!");
+        fortunes.add("You will have a great time!");
+        fortunes.add("You will walk in a circle 3 times!");
+        fortunes.add("You will eat one big yummy egg!");
+        fortunes.add("You will meet spiderman!");
+        fortunes.add("You will meet batman!");
+        fortunes.add("You will meet Homer Simpson!");
+        fortunes.add("You will go to the store buy a thing!");
+        fortunes.add("You will be a millionaire!");
+        fortunes.add("You will be a superhero!");
+        fortunes.add("You will be a rock star!");
+        fortunes.add("You will be a supermodel!");
     }
 }
